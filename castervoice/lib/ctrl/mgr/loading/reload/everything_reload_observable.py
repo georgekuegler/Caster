931fd1410b63098ae329f25f03297e38a67c32c9
George Kuegler <georgekuegler@gmail.com>
2023-04-06 13:46:21 -0400

    fix toast

--- castervoice/lib/ctrl/mgr/loading/reload/everything_reload_observable.py
+++ castervoice/lib/ctrl/mgr/loading/reload/everything_reload_observable.py
@@ -1,11 +1,20 @@
 from dragonfly import Function, MappingRule, Key
 
-from castervoice.lib.ctrl.mgr.loading.reload.base_reload_observable import BaseReloadObservable
+from castervoice.lib.ctrl.mgr.loading.reload.base_reload_observable import (
+    BaseReloadObservable,
+)
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 
 from time import sleep
 from natlink import setMicState
-from library.log import toast
+
+try:
+    from library import notify
+
+    use_toast = True
+except:
+    use_toast = False
+
 
 class Listener:
     def __init__(self):
@@ -19,20 +28,24 @@ class Listener:
 
     def notify_user(self):
         msg = "caster" if self.reloaded_files else "nothing"
-        toast.toast(msg, "reloaded")
+        if use_toast:
+            notify.toast(msg, "reloaded")
+
 
 class ManualReloadObservable(BaseReloadObservable):
     """
     Allows for reloading changed files on command.
     """
+
     def __init__(self):
         super(ManualReloadObservable, self).__init__()
 
-        '''
+        """
         This class itself will never be reloaded, but it can
         be registered like the other rules and so can have
         transformers run over it, etc.
-        '''
+        """
+
         class ManualGrammarReloadRule(MappingRule):
             mapping = {
                 "reload all rules": Function(self.reload_all_rules),
@@ -43,7 +56,6 @@ class ManualReloadObservable(BaseReloadObservable):
         self.listener = Listener()
         self.register_listener(self.listener)
 
-    
     def reload_all_rules(self):
         self.listener.reset()
         self._update()
@@ -60,8 +72,9 @@ class ManualReloadObservable(BaseReloadObservable):
         self.listener.reset()
         self._update()
         self.listener.notify_user()
-    
+
     def get_loadable(self):
-        details = RuleDetails(name="caster manual grammars reload command rule",
-                              watch_exclusion=True)
+        details = RuleDetails(
+            name="caster manual grammars reload command rule", watch_exclusion=True
+        )
         return self._rule_class, details
