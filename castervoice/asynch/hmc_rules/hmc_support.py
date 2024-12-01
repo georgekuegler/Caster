0d62603b35315ed61c318e9e5b299185e6745b8a
George Kuegler <georgekuegler@gmail.com>
2024-11-30 23:23:32 -0500

    supress unecessary warning

--- castervoice/asynch/hmc_rules/hmc_support.py
+++ castervoice/asynch/hmc_rules/hmc_support.py
@@ -3,7 +3,3 @@ from castervoice.lib import control, settings, printer
 
 def kill():
     control.nexus().comm.get_com("hmc").kill()
-
-
-if not settings.settings(["miscellaneous", "hmc"]):
-    printer.out("WARNING: Tk Window controls have been disabled -- this is not advised!")
